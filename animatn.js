4c7d34d4c0977c77c0c37f039eb60ea3e4d33684
rohan <pr.rsaikia@gmail.com>
2020-08-23 00:54:59 +0530

    major modifications

--- animatn.js
+++ animatn.js
@@ -1,61 +1,45 @@
-window.addEventListener("scroll", myAnimation1);
-window.addEventListener("resize", myAnimation2);
-window.onload=load_page_event_handler();
-
 var wind_size_height=window.innerHeight;
 var wind_size_width=window.innerWidth;
 var scroll_val;
 var rot1_val;
 var rot2_val;
-var navgn_width=20;
 var flag_view;
 var navgn_scroll;
+var scroll_element;
 var myTimer; 
 var flag_timer=0; 
 var rot1_val1_m=0;
 var rot1_val2_m=0;
 
-
-function mobile_view()
-{
-    console.log("Mobile view");
-    flag_view="mv";
-    myTimer = setInterval("timer_handler()", 50);
-    document.querySelector(".main_body_div").classList.remove("main_body_div_desktop");
-    document.querySelector(".gear1").classList.remove("gear1_d");
-    document.querySelector(".gear2").classList.remove("gear2_d");
-    document.querySelector(".brand").classList.remove("brand_d");
-    document.querySelector(".gear_logo").classList.remove("gear_logo_d");
-    document.querySelector(".master_class_logo").classList.remove("master_class_logo_d");
-    document.querySelector(".navgn_panel").classList.remove("navgn_panel_d");
-}
+window.onload=load_page_event_handler();
 
 function desktop_view()
 {
     console.log("Desktop view");
     flag_view="dv";
 
-    if(flag_timer==1)               //if timer enabled,stop timer
+    if(flag_timer==1)                                       //if timer enabled,stop timer
     {
         flag_timer=0;
         clearInterval(myVar);
     }
+    // document.querySelector(".master_class_logo").classList.remove("master_class_logo_d");
+    document.querySelector(".expand_button").style.visibility="visible";
+}
 
-    document.querySelector(".main_body_div").classList.remove("main_body_div_mobile");
-    document.querySelector(".gear1").classList.remove("gear1_m");
-    document.querySelector(".gear2").classList.remove("gear2_m");
-    document.querySelector(".brand").classList.remove("brand_m");
-    document.querySelector(".gear_logo").classList.remove("gear_logo_m");
-    document.querySelector(".master_class_logo").classList.remove("master_class_logo_m");
-    document.querySelector(".navgn_panel").classList.remove("navgn_panel_m");
+function mobile_view()
+{
+    console.log("Mobile view");
+    flag_view="mv";
+    myTimer = setInterval("timer_handler()", 50);
 }
 
 function timer_handler(){                                   //mobile_view_logo_animation
     flag_timer=1;
     document.querySelector(".gear1").style.transform=`rotate(-${rot1_val1_m+=20}deg)`;
     document.querySelector(".gear2").style.transform=`rotate(${rot1_val2_m+=40}deg)`;
-    if(rot1_val1_m>=360) rot1_val1_m=0;
-    if(rot1_val2_m>=360) rot1_val2_m=0;
+    if(rot1_val1_m>=359) rot1_val1_m=0;
+    if(rot1_val2_m>=359) rot1_val2_m=0;
 }
 
 
@@ -65,24 +49,24 @@ function load_page_event_handler()  //730 539
 }
 
 
-function myAnimation2()
+window.addEventListener("resize", function myAnimation2()
 {    
     wind_size_height=window.innerHeight;
     wind_size_width=window.innerWidth;
     location.reload();   
-}
+});
 
 
-function myAnimation1()
+window.addEventListener("scroll", function myAnimation1()
 {
-    var scroll_element=window.scrollY;
+    scroll_element=window.scrollY;
     rot1_val=(scroll_element/6);
     rot2_val=(scroll_element/3);
     navgn_scroll=scroll_element/4;
 
     if(flag_view=="dv")
     {
-        console.log("Desktop_scroll");
+        // console.log("Desktop_scroll");
 
         if(scroll_element>scroll_val)   //scroll down
         {        
@@ -115,11 +99,11 @@ function myAnimation1()
     }
     else
     {
-        console.log("mobile_scroll");
+        // console.log("mobile_scroll");
     }
 
     scroll_val=window.scrollY;
-}
+});
 
 
 
@@ -129,7 +113,7 @@ function fx_body_load()
 
     (wind_size_width<600)?mobile_view():desktop_view();
     
-    var scroll_element=window.scrollY;
+    scroll_element=window.scrollY;
     navgn_scroll=scroll_element/4;
 
     if(flag_view=="dv")
@@ -142,4 +126,10 @@ function fx_body_load()
     }
     
     document.querySelector(".main_body_div").style.visibility="visible";
+}
+
+
+function scroll_to()            //Scroll function for the navigation panel expansion button
+{
+   window.scrollTo( 0,400);
 }
